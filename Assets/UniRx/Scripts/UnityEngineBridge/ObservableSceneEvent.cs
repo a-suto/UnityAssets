052fe74ff8c0436340d23787cf08c7f1330c4c65
a-suto <a-suto@magicant.co.jp>
2020-09-27 18:07:29 +0900

    シーン周りのイベントをIObservableとして提供するObservableSceneEventクラス追加
    SEE:
    https://qiita.com/su10/items/93977e0b95449ec1b944

---
+++ Assets/UniRx/Scripts/UnityEngineBridge/ObservableSceneEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace UniRx
+{
+    public static class ObservableSceneEvent
+    {
+        public static IObservable<Tuple<Scene, Scene>> ActiveSceneChangedAsObservable()
+        {
+            return Observable.FromEvent<UnityAction<Scene, Scene>, Tuple<Scene, Scene>>(
+                h => (x, y) => h(Tuple.Create(x, y)),
+                h => SceneManager.activeSceneChanged += h,
+                h => SceneManager.activeSceneChanged -= h
+            );
+        }
+
+        public static IObservable<Tuple<Scene, LoadSceneMode>> SceneLoadedAsObservable()
+        {
+            return Observable.FromEvent<UnityAction<Scene, LoadSceneMode>, Tuple<Scene, LoadSceneMode>>(
+                h => (x, y) => h(Tuple.Create(x, y)),
+                h => SceneManager.sceneLoaded += h,
+                h => SceneManager.sceneLoaded -= h
+            );
+        }
+
+        public static IObservable<Scene> SceneUnloadedAsObservable()
+        {
+            return Observable.FromEvent<UnityAction<Scene>, Scene>(
+                h => h.Invoke,
+                h => SceneManager.sceneUnloaded += h,
+                h => SceneManager.sceneUnloaded -= h
+            );
+        }
+    }
+}
